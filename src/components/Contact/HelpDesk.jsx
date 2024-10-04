8db8a8c5e06cbf2b66e744d39ab8d3d04f8187b5
revon01 <rishabh.mishra@gmail.com>
2024-10-04 21:15:48 +0530

    Redux-toolkit

--- src/components/Contact/HelpDesk.jsx
+++ src/components/Contact/HelpDesk.jsx
@@ -9,43 +9,43 @@ function HelpDesk() {
         <div>
         <h2>Browse our help desk</h2>
         </div>
-        <div className='main-container'>
+        <div className='main-container-div'>
        
-            <div className='help-item'>
+            <div className='help-desk-item'>
                 <img src={HelpDesk.image1} alt=""/>
                 <h3>Size Guide</h3>
                 <p>Ensure your customers get the right fit</p>
                  <p>View More</p>
                  </div>
 
-             <div className='help-item'>
+             <div className='help-desk-item'>
                 <img src={HelpDesk.image2} alt=""/>
                 <h3>Loyalty Program</h3>
                 <p>Offer exclusive rewards</p>
                  <p>View More</p>
                  </div>
-                 <div className='help-item'>
+                 <div className='help-desk-item'>
                 <img src={HelpDesk.image3} alt=""/>
                 <h3>Order & Shipping</h3>
                 <p>Looking for your order status</p>
                  <p>View More</p>
                  </div>
 
-                 <div className='help-item'>
+                 <div className='help-desk-item'>
                 <img src={HelpDesk.image4} alt=""/>
                 <h3>Payment & Gift cards</h3>
                 <p>Wonder of what payment we accept?</p>
                  <p>View More</p>
                  </div>
 
-                 <div className='help-item'>
+                 <div className='help-desk-item'>
                 <img src={HelpDesk.image5} alt=""/>
                 <h3>Discount Codes</h3>
                 <p>Have a discount you like to use?</p>
                  <p>View More</p>
                  </div>
 
-                 <div className='help-item'>
+                 <div className='help-desk-item'>
                 <img src={HelpDesk.image6} alt=""/>
                 <h3>Store Location</h3>
                 <p>Want to find a store near you?</p>
